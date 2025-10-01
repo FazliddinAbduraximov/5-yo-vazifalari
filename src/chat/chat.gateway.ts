cbb40357127ae20b0c3f16e50d55942f5f4adaa5
Fazliddin <fazliddin.abdurahimov9602@gmail.com>
2025-10-01 07:47:39 +0500

    socket

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -14,7 +14,13 @@ export class ChatGateway {
   server: Server;
 
   @SubscribeMessage('send_message')
-  handleMessage(@MessageBody() message: { sender: string; text: string }) {
+  handleMessage(@MessageBody() message: { sender: string; text: string; time: number }) {
     this.server.emit('receive_message', message);
+  }
+
+  @SubscribeMessage('location_update')
+  handleLocation(@MessageBody() loc: { sender: string; lat: number; lng: number }) {
+    this.server.emit('location_update', loc);
+    this.server.emit('notify', { title: 'Joylashuv yangilandi', body: `${loc.sender} joy yubordi` });
   }
 }
